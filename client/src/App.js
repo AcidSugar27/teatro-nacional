7166fe93f188f75db144853bf610bfcf642f6c52
AcidSugar27 <eliasamarante27@gmail.com>
2024-11-08 11:40:21 -0500

    declaracion y englobe de app.js para compras con stripe

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,48 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import CarteleraForm from "./components/cartelera/CarteleraForm";
+import CarteleraList from "./components/cartelera/CarteleraList";
+import Menu from './components/Navbar';
+import { Container } from '@mui/material';
+import SalaForm from './components/salas/SalaForm';
+import SalaList from './components/salas/SalaList';
+import Login from './components/auth/login';
+import Registrar from './components/auth/registrar';
+import ForgotPassword from './components/auth/olvidarps';
+import ResetPassword from './components/auth/reiniciarps';
+import VerifyEmail from './components/auth/verificacion';
+import CarteleraDetails from './components/cartelera/CarteleraDetails';
+import TicketCompra from './components/compra/TicketCompra';
 
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import CarteleraForm from "./components/cartelera/CarteleraForm"
-import CarteleraList from "./components/cartelera/CarteleraList"
-import  Menu from './components/Navbar'
-import {Container} from '@mui/material'
-import SalaForm from './components/salas/SalaForm'
-import SalaList from './components/salas/SalaList'
-import Login from './components/auth/login'
-import Registrar from './components/auth/registrar'
-import ForgotPassword from './components/auth/olvidarps'
-import ResetPassword from './components/auth/reiniciarps'
-import VerifyEmail from './components/auth/verificacion'
-import CarteleraDetails from './components/cartelera/CarteleraDetails'
+// Cargar Stripe con tu clave pública
+const stripePromise = loadStripe('pk_test_51QIsRaHlnQXc8B6sqQ1JcAwZx9g1LpMoyeDGIkRPVO8ulbVlymuetk88w7yexjWv0988pMD1Zpvi8X2TOI17BAqI00BqyCi3Hw');
 
 export default function App() {
   return (
-    <BrowserRouter >
-    <Menu/>
-     <Container >
-
-     
-      <Routes>
-       <Route path='/' element={<CarteleraList/>} /> 
-       <Route path='/cartelera/new' element={<CarteleraForm/>} /> 
-       <Route path='/cartelera/:id/edit' element={<CarteleraForm/>} />
-       <Route path="/salas" element={<SalaList />} />
-       <Route path="/sala/new" element={<SalaForm />} />
-       <Route path="/sala/:id/edit" element={<SalaForm />} />
-       <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Registrar/>} /> 
-      <Route path="/forgot-password" element={<ForgotPassword/>} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route path="/cartelera/:id/" element={<CarteleraDetails />} />
-      </Routes>
-     </Container>
+    <BrowserRouter>
+      <Menu />
+      <Container>
+        <Elements stripe={stripePromise}>  {/* Agregar Elements aquí */}
+          <Routes>
+            <Route path='/' element={<CarteleraList />} />
+            <Route path='/cartelera/new' element={<CarteleraForm />} />
+            <Route path='/cartelera/:id/edit' element={<CarteleraForm />} />
+            <Route path="/salas" element={<SalaList />} />
+            <Route path="/sala/new" element={<SalaForm />} />
+            <Route path="/sala/:id/edit" element={<SalaForm />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Registrar />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/verify-email" element={<VerifyEmail />} />
+            <Route path="/cartelera/:id/" element={<CarteleraDetails />} />
+            <Route path="/cartelera/:id/comprar" element={<TicketCompra />} />
+          </Routes>
+        </Elements> {/* Cerrar Elements aquí */}
+      </Container>
     </BrowserRouter>
-
-  )
+  );
 }
+
